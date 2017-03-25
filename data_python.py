8c7593ad5356fba18d9473c8e86bedcb68882201
user.name <chuan.ma@qq.com>
2017-03-25 22:48:00 +0800

    add file

---
+++ data_python.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# %%
+import numpy as np
+
+
+# %%
+data1 = [6, 7.5, 8, 0, 1]
+arr1 = np.array(data1)
+data2 = [[1,2,3,4], [5,6,7,8]]
+arr2 = np.array(data2)
+arr2.ndim # 查看arr2的维数
+arr2.shape
+arr2.dtype
+np.zeros(10)
