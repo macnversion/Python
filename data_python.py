69e801f5c301ef0144ffb75996798eb8242c5658
macnversion <chuan.ma@qq.com>
2017-03-31 07:48:56 +0800

    update

--- data_python.py
+++ data_python.py
@@ -2,6 +2,8 @@
 # %%
 import numpy as np
 from numpy import random
+import pandas as pd
+from pandas import Series, DataFrame
 
 
 # %%
@@ -27,3 +29,12 @@ y = np.array([2.1, 2.2, 2.3, 2.4, 2.5])
 con = np.array([True, False, True, False, True])
 result1 = [(x1 if c else y1) for x1, y1, c in zip(x, y, con)]
 result2 = np.where(con, x, y)
+
+frame = DataFrame(np.arange(16).reshape(4, 4),
+                  index=['a', 'b', 'c', 'd'],
+                  columns=['Ohio', 'Texas', 'Cali', 'New York'])
+f = lambda x: x.max() - x.min()
+frame.apply(f)
+frame.apply(f, axis=1)
+frame.sort_index()
+frame.sort_index(axis=1)
