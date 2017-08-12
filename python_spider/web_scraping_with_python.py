d57ee4030792911163187a6768284f4329220c87
macnversion <chuan.ma@qq.com>
2017-08-12 14:59:38 +0800

    new

---
+++ python_spider/web_scraping_with_python.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# %%
+import urllib2
+import re
+import itertools
+
+# %%
+url = r'http://example.webscraping.com/'
+
+
+def download(url, user_agent='wswp', num_retries=2):
+    print 'Downloading url:', url
+    headers = {'User-agent': user_agent}
+    request = urllib2.Request(url, headers=headers)
+    try:
+        html = urllib2.urlopen(request).read()
+    except urllib2.URLError as e:
+        print 'Download error:', e.reason
+        html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code <= 600:
+                # retry 5xx http error
+                return download(url, num_retries-1)
+    return html
+
+def crawl_sitemap(url):
+    # download the sitemap
+    sitemap = download(url)
+    # extract the sitemap links
+    links = re.findall('<loc>(.*)</loc>', sitemap)
+    for link in links:
+        html = download(link)
+
+for page in itertools.count(1):
+    url = 'http://example.webscraping.com/places/default/view/-%d' % page
+    html = download(url)
+    if html is None:
+        break
+    elif page > 5:
+        break
+    else:
+        pass
+
+def link_crawler(seed_url, link_regex):
+    crawl_queue = [seed_url]
+    while crawl_queue:
+        url = crawl_queue.pop()
+        html = download(url)
+
+        for link in get_links(html):
+            if re.match(link_regex, link):
+                crawl_queue.append(link)
+
+
+def get_links(url):
+    '''return a list of links from html'''
+    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
+    return webpage_regex.findall(html)
