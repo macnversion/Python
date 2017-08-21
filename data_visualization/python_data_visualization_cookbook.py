6390af36d7d51f696f62c97a96ae4ed545db7368
user.name <chuan.ma@qq.com>
2017-08-21 08:30:46 +0800

    vis

---
+++ data_visualization/python_data_visualization_cookbook.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import os
+import platform
+
+# 工作路径设定
+if 'Windows' in platform.platform():
+    data_path = r'D:/WorkSpace/CodeSpace/Code.Data/Python/'
+else:
+    data_path = r'/Users/machuan/CodeSpace/Code.Data/python/'
+
