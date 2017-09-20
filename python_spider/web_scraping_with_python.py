be263c6e32d110caa1b776329bd02af866811a5a
user.name <chuan.ma@qq.com>
2017-09-20 15:47:26 +0800

    update

--- python_spider/web_scraping_with_python.py
+++ python_spider/web_scraping_with_python.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
 # 用python写网络爬虫
-# %%
+# %% 使用的库
 import urllib2
 import re
 import itertools
 import builtwith
 import whois
+import urlparse
+import robotparser
+import datetime
 
-# %%
+# %% 爬虫
 url = r'http://example.webscraping.com/'
 
-# %% 查看网站所所使用的技术和所有者
+# 查看网站所所使用的技术和所有者
 builtwith.parse(url)
 whois.whois(url)
 
 
-def download(url, user_agent='wswp', num_retries=2):
+def download(url, user_agent='wswp', proxy = None, num_retries=2):
     print 'Downloading url:', url
     headers = {'User-agent': user_agent}
     request = urllib2.Request(url, headers=headers)
+
+    opener = urllib2.build_opener()
+    if proxy:
+        proxy_params = {urlparse.urlparse(url).scheme: proxy}
+        opener.add_handler(urllib2.ProxyHandler(proxy_params))
     try:
         html = urllib2.urlopen(request).read()
     except urllib2.URLError as e:
@@ -48,18 +56,58 @@ for page in itertools.count(1):
     else:
         pass
 
+
 def link_crawler(seed_url, link_regex):
     crawl_queue = [seed_url]
+    # keep track which url has been seen before
+    seen = set(crawl_queue)
     while crawl_queue:
         url = crawl_queue.pop()
         html = download(url)
 
-        for link in get_links(html):
-            if re.match(link_regex, link):
-                crawl_queue.append(link)
+        rp = robotparser.RobotFileParser()
+        rp.set_url(url + '/robot.txt')
+        if rp.can_fetch(user_agent, url):
+            for link in get_links(html):
+                if re.match(link_regex, link):
+                    link = urlparse.urljoin(seed_url, link)
+                    if link not in seen:
+                        seen.add(link)
+                        crawl_queue.append(link)
+        else:
+            print 'Blocked by robots.txt:', url
+    return crawl_queue
 
 
-def get_links(url):
+def get_links(html):
     '''return a list of links from html'''
     webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
     return webpage_regex.findall(html)
+
+class Throttle:
+    '''Add a delay between downloads to the same domain'''
+
+    def __init__(self, delay):
+        # amount of delay between downloads for each domain
+        self.delay = delay
+        # timestamp of when a domain was last accessed
+        self.domains = {}
+
+    def wait(self, url):
+        domian = urlparse.urlparse(url).netloc
+        last_accessed = self.domains.get(domain)
+
+        if self.delay > 0 and last_accessed is nit None:
+            sleep_secs = self.delay - (datetime.datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                # domain has been accessed recently
+                # so need to sleep
+                time.sleep(sleep_secs)
+
+        self.domains[domian] = datetime.datetime.now()
+
+
+# %% 数据抓取
+ url = r'http://example.webscraping.com/places/default/view/239'
+ html = download(url)
+ result = re.findall('<td class="w2p_fw">(.*?)</td>', html)
