b9c469e7d1ee2be4d7d7d680b853f7377e45e3e3
macnversion <chuan.ma@qq.com>
2017-12-11 08:53:25 +0800

    update performance

--- python_spider/web_scraping_with_python/chapter02/performacnce.py
+++ python_spider/web_scraping_with_python/chapter02/performacnce.py
@@ -15,4 +15,30 @@ fields = ('area', 'population', 'iso', 'country', 'capital', 'continent', 'tld',
 def regex_scraper(html):
     results = {}
     for field in fields:
-        results[field] = re.search()
+        results[field] = re.search('<tr id="places_{}__row">.*?<td class="w2p_fw">(.*?)</td>'.format(field),
+                                   html).groups()[0]
+        return results
+
+
+def beautifuk_soup_scraper(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    results = {}
+    for field in fields:
+        results[field] = soup.find('table').find('tr', id='places_{}__row'.format(field)).find('td',
+                                                                                              class_='w2p_fw').text
+        return results
+
+
+def lxml_scraper(html):
+    tree = lxml.html.fromstring(html)
+    results = {}
+    for field in fields:
+        results[field] = tree.cssselect('table > tr#places_{}__row > td.w2p_fw'.format(field))[0].text_content()
+    return results
+
+
+def main():
+    times = {}
+    html = urllib2.urlopen('http://example.webscraping.com/places/default/view/Aland-Islands-2').read()
+    NUM_ITERATIONS = 1000 # 每个爬虫测试1000次
+    for name, scraper in ()
